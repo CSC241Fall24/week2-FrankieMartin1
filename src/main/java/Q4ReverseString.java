df43538f8fef0b7518a07a383721b5311ec62c2a
FrankieMartin1 <158102149+FrankieMartin1@users.noreply.github.com>
2024-09-06 06:16:16 -0400

    Update Q4ReverseString.java

--- src/main/java/Q4ReverseString.java
+++ src/main/java/Q4ReverseString.java
@@ -1,9 +1,13 @@
 // src/main/java/Q4ReverseString.java
-
 public class Q4ReverseString {
 
     public static String reverse(String s) {
-        // TODO: Implement the recursive function to reverse the string
-        return "";
+        // Base case: If the string is empty or has only one character, return it as it is
+        if (s.length() <= 1) {
+            return s;
+        }
+
+        // Recursive case: Reverse the substring starting from index 1 and add the first character at the end
+        return reverse(s.substring(1)) + s.charAt(0);
     }
 }
