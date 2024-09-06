b0629ba1039b1875b2729b7820055d2fbf84b31f
FrankieMartin1 <158102149+FrankieMartin1@users.noreply.github.com>
2024-09-06 06:46:09 -0400

    Update Q1WeirdFunction.java

--- src/main/java/Q1WeirdFunction.java
+++ src/main/java/Q1WeirdFunction.java
@@ -1,26 +1,38 @@
 public class Q1WeirdFunction {
 
-    // Recursive implementation of factorial
+    // Recursive implementation
     public static int fRecursive(int n) {
-        if (n <= 1) {
-            return 1;
-        }
-        return n * fRecursive(n - 1);
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        return fRecursive(n - 1) + fRecursive(n - 2) + fRecursive(n - 3);
     }
 
-    // Iterative implementation of factorial
+    // Iterative implementation
     public static int fIterative(int n) {
-        int result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i;
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         }
-        return result;
+
+        return dp[n];
     }
 
     public static void main(String[] args) {
         // Test the implementations
-        int num = 5;
-        System.out.println("Factorial of " + num + " (recursive): " + fRecursive(num));
-        System.out.println("Factorial of " + num + " (iterative): " + fIterative(num));
+        int[] testValues = {0, 1, 2, 3, 4};
+        for (int num : testValues) {
+            System.out.println("fRecursive(" + num + ") = " + fRecursive(num));
+            System.out.println("fIterative(" + num + ") = " + fIterative(num));
+        }
     }
 }
+
