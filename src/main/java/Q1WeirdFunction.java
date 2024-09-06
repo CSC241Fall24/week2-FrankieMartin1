ba59eb90fd4f09adfdcce6854cb0573b4522337e
FrankieMartin1 <158102149+FrankieMartin1@users.noreply.github.com>
2024-09-06 06:11:41 -0400

    Update Q1WeirdFunction.java

--- src/main/java/Q1WeirdFunction.java
+++ src/main/java/Q1WeirdFunction.java
@@ -1,14 +1,35 @@
-// src/main/java/Q1WeirdFunction.java
-
 public class Q1WeirdFunction {
 
+    // Recursive function implementation
     public static int fRecursive(int n) {
-        // TODO: Implement the recursive function
-        return 0;
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fRecursive(n - 1) + (n - 1) * fRecursive(n - 2);
+        }
     }
 
+    // Iterative function implementation
     public static int fIterative(int n) {
-        // TODO: Implement the iterative function
-        return 0;
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+
+        int f0 = 0;
+        int f1 = 1;
+        int result = 0;
+
+        for (int i = 2; i <= n; i++) {
+            result = f1 + (i - 1) * f0;
+            f0 = f1;
+            f1 = result;
+        }
+
+        return result;
     }
 }
