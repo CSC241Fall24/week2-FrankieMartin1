282b863afa827b69cd4149161bd9f6bbc80ecd36
FrankieMartin1 <158102149+FrankieMartin1@users.noreply.github.com>
2024-09-09 11:03:24 -0400

    Update Q1WeirdFunction.java

--- src/main/java/Q1WeirdFunction.java
+++ src/main/java/Q1WeirdFunction.java
@@ -2,17 +2,17 @@ public class Q1WeirdFunction {
 
     // Recursive implementation
     public static int fRecursive(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-        return fRecursive(n - 1) + fRecursive(n - 2) + fRecursive(n - 3);
+        if(n<3) {
+            return n;
+        }
+        return fRecursive(n - 1) + 2 * fRecursive(n - 2) + 3 *  fRecursive(n - 3);
     }
 
     // Iterative implementation
     public static int fIterative(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        if (n == 2) return 2;
+        if(n<3){
+            return n;
+        }
 
         int[] dp = new int[n + 1];
         dp[0] = 0;
@@ -20,19 +20,9 @@ public class Q1WeirdFunction {
         dp[2] = 2;
 
         for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+            dp[i] = dp[i - 1] + 2* dp[i - 2] +3* dp[i - 3];
         }
 
         return dp[n];
     }
-
-    public static void main(String[] args) {
-        // Test the implementations
-        int[] testValues = {0, 1, 2, 3, 4};
-        for (int num : testValues) {
-            System.out.println("fRecursive(" + num + ") = " + fRecursive(num));
-            System.out.println("fIterative(" + num + ") = " + fIterative(num));
-        }
-    }
 }
-
